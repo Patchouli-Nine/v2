e4594c0019fa918675b0e315767d04620966d79e
Patchouli Nine <62474037+Patchouli-Nine@users.noreply.github.com>
2022-04-15 18:59:21 +0800

    .

--- v2ray.sh
+++ v2ray.sh
@@ -277,7 +277,6 @@ getData() {
             if [[ -z "${res}" ]]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
                 colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
-                exit 1
             fi
         fi
     fi
